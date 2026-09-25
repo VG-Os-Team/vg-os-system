bfa83b1337889584ae9fd5b009fddbf69ea7565b
Melquisedeque <melq4467ll@gmail.com>
2026-09-25 19:09:41 -0300

    Fix version and make iso

--- version.h
+++ version.h
@@ -1,8 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
+// Favor, modificar versão quando fizer modificações no codigo
+// Exemplo: 0.3.9 -> 0.4.0
+
 #define OS_NAME "VG OS"
-#define OS_VERSION "0.3.9"
+#define OS_VERSION "0.4.0"
 #define OS_STAGE "beta"
 
 #define OS_BANNER OS_NAME " Versao " OS_VERSION "-" OS_STAGE
